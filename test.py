e57f6f978f64990d69a1ce23be3f80dfb3e59162
itglueguy <38425660+itglueguy@users.noreply.github.com>
2022-05-30 15:04:58 -0700

    fixing bugs

---
+++ test.py
@@ -0,0 +1,16 @@
+# initiate our variable
+make_more_money = 0
+    
+learn_to_program = 'no'
+great_politician = 'Yes'
+  
+if learn_to_program == 'Yes':
+  make_more_money = 1
+  print("Learning to progam will likely make you more money in the long term if you are persistant");
+elif learn_to_program == 'No':
+  if great_politician == 'Yes':
+    print("The reality is that if you navigate politics well, you can probably do better than a programmer in the long term");
+    make_more_money = 1
+  else:
+    print("I am satisfied with clicking to do my work and dont want to deal with politics and that is good enough");
+    make_more_money = 0
